3c304358bfaf200e004cf020e974f1dd7815e3e7
Oliver Greenwood <greenwood672.dev@gmail.com>
2022-02-03 17:49:57 +0000

    Disconnecting update

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ const io = socket(server);
 
 const roomLyrics = {}
 
-
 function newRandomSong(room) {
   playlist = songs.filter(p => p["name"] == "Best")[0];
   let song = playlist.tracks[Math.floor(Math.random() * playlist.tracks.length)];
@@ -39,8 +38,8 @@ function newRandomSong(room) {
     artist: song.artist,
     optimizeQuery: true
   };
-  
-  
+
+
   getLyrics(options).then((lyrics) => {
     if (lyrics == null) {
       newRandomSong(room);
@@ -52,17 +51,29 @@ function newRandomSong(room) {
       lyrics: lyrics.split("\n")
     }
   });
+
+  roomLyrics[room].peopleSucceed = [];
+
 }
 
 function sendLyrics(socketid) {
 
     const p_user = get_Current_User(socketid);
 
-    let song = roomLyrics[p_user.room].song;
+    let song;
+
+    try {
+      song = roomLyrics[p_user.room].song;
+    } catch {
+      return;
+    }
 
     if (song == undefined || song.lyrics.length == 1) {
-      console.log("Resetting");
-      clearInterval(roomLyrics[p_user.room].interval)
+      reset(socketid);
+      return;
+    }
+
+    if (roomLyrics[p_user.room].peopleSucceed.length === roomLyrics[p_user.room].users.length) {
       reset(socketid);
       return;
     }
@@ -82,14 +93,18 @@ function sendLyrics(socketid) {
 }
 
 function reset(socketid) {
+  console.log("Resetting");
 
   const p_user = get_Current_User(socketid);
+
+  clearInterval(roomLyrics[p_user.room].interval)
+  roomLyrics[p_user.room].interval = false;
 
   newRandomSong(p_user.room);
 
   io.to(p_user.room).emit("reset");
 
-  roomLyrics[p_user.room].interval = setInterval(() => sendLyrics(socketid), 5000)
+  roomLyrics[p_user.room].interval = setInterval(() => sendLyrics(socketid), 5000);
 
 }
 
@@ -118,13 +133,15 @@ io.on("connection", (socket) => {
     });
     
     if (!roomLyrics.hasOwnProperty(p_user.room)) {
-      roomLyrics[p_user.room] = {};
-      newRandomSong(p_user.room);
-
-      setTimeout(() => {
-        roomLyrics[p_user.room].interval = setInterval(() => sendLyrics(socket.id), 50)
-      }, 5000);
+      roomLyrics[p_user.room] = {
+        users: [],
+        interval: false,
+        peopleSucceed: []
+      };
     }
+
+    roomLyrics[p_user.room].users.push(p_user.id)
+
   });
 
   //user sending message
@@ -132,11 +149,40 @@ io.on("connection", (socket) => {
     //gets the room user and the message sent
     const p_user = get_Current_User(socket.id);
 
+    if (roomLyrics[p_user.room].peopleSucceed.includes(p_user.id)) {
+
+      socket.emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `You cannot chat; You've already guessed the song`,
+      });
+
+      return;
+    }
+
+    if (text.startsWith("/")) {
+      text = text.slice(1, text.length);
+      if (text == "skip") {
+        roomLyrics[p_user.room].peopleSucceed.push(p_user.id);
+      } else if (text == "start" && !roomLyrics[p_user.room].interval) {
+
+        console.log(text);
+        
+        newRandomSong(p_user.room);
+
+        setTimeout(() => {
+          roomLyrics[p_user.room].interval = setInterval(() => sendLyrics(socket.id), 5000)
+        }, 5000);
+
+      }
+      return;
+    }
+
     try {
 
-    songFiltered = (roomLyrics[p_user.room].song.name.split("").filter(letter => {
+      songFiltered = (roomLyrics[p_user.room].song.name.split("").filter(letter => {
         n = letter.charCodeAt(0);
-        if (n == 32 || (64 < n && n < 91) || (96 < n && n < 123)) {
+        if (n == 32 || (47 < n && n < 57) || (64 < n && n < 91) || (96 < n && n < 123)) {
           return letter;
         }
         return;
@@ -145,11 +191,13 @@ io.on("connection", (socket) => {
       console.log(songFiltered);
 
       if (text.toLowerCase() == songFiltered) {
+
         io.to(p_user.room).emit("message", {
           userId: p_user.id,
-          username: "god",
+          username: "GOD",
           text: `${p_user.username} got it`,
         });
+        roomLyrics[p_user.room].peopleSucceed.push(p_user.id);
         return;
 
       }
@@ -162,29 +210,34 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("start", () => {
-
-    const p_user = get_Current_User(socket.id);
-
-    newRandomSong(p_user.room);
-
-    setInterval(() => sendLyrics(socket.id), 5000);
-
-  })
-
-  
-
   //when the user exits the room
   socket.on("disconnect", () => {
     //the user is deleted from array of users and a left room message displayed
     const p_user = user_Disconnect(socket.id);
+    console.log(p_user);
 
     if (p_user) {
-      io.to(p_user.room).emit("message", {
-        userId: p_user.id,
-        username: p_user.username,
-        text: `${p_user.username} has left the chat`,
-      });
+
+      if (roomLyrics.hasOwnProperty(p_user.room)) {
+        try {
+          roomLyrics[p_user.room].users.splice(roomLyrics[p_user.room].users.indexOf(p_user.id), 1);
+          roomLyrics[p_user.room].peopleSucceed.splice(roomLyrics[p_user.room].peopleSucceed.indexOf(p_user.id), 1);
+        } catch (e) {
+          console.log(e);
+        }
+
+        if (roomLyrics[p_user.room].users.length == 0) {
+          clearInterval(roomLyrics[p_user.room].interval);
+          delete roomLyrics[p_user.room];
+        } else {
+          io.to(p_user.room).emit("message", {
+            userId: p_user.id,
+            username: p_user.username,
+            text: `${p_user.username} has left the chat`,
+          });
+        }
+
+      }
     }
   });
 });
